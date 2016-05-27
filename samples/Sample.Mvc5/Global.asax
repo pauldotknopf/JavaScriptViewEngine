4f48f1c471fd68eb4b4e30c7620aff94568a1888
Paul Knopf <pauldotknopf@gmail.com>
2016-05-26 22:33:47 -0400

    Added a sample Mvc5 project. Next step, get our library working with it.

---
+++ samples/Sample.Mvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sample.Mvc5.MvcApplication" Language="C#" %>
---
+++ samples/Sample.Mvc5/ViewModels/GreetingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Sample.Mvc5.ViewModels
+{
+    public class GreetingViewModel
+    {
+        [JsonProperty("greeting")]
+        public string Greeting { get; set; }
+    }
+}
