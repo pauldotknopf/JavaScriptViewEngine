a7c85480a6215956246e96585134894ccd3b07cd
Paul Knopf <pauldotknopf@gmail.com>
2016-07-21 01:05:21 -0400

    Improved how we register the engine to make the registration more explicit.

--- src/JavaScriptViewEngine/Extensions.cs
+++ src/JavaScriptViewEngine/Extensions.cs
@@ -17,6 +17,9 @@ using Microsoft.Extensions.Options;
 
 namespace JavaScriptViewEngine
 {
+    /// <summary>
+    /// Some extensions
+    /// </summary>
     public static class Extensions
     {
         /// <summary>
@@ -42,18 +45,31 @@ namespace JavaScriptViewEngine
         /// <summary>
         /// Add the services required to use a render engine, a pool, etc.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
         /// <param name="services">The services.</param>
+        [Obsolete("Use the overload that uses the 'JsEngineServiceBuilder'.")]
         public static void AddJsEngine(this IServiceCollection services)
         {
-            services.TryAddEnumerable(ServiceDescriptor.Transient<IConfigureOptions<MvcViewOptions>, JavaScriptViewEngineMvcViewOptionsSetup>());
-            services.TryAddTransient<IFileWatcher, FileWatcher>();
-            services.TryAddTransient<IRenderEnginePool, RenderEnginePool>();
-            services.TryAddTransient<IRenderEngineBuilder, NodeRenderEngineBuilder>();
-            services.TryAddSingleton<IRenderEngineFactory, PooledRenderEngineFactory>();
-            services.TryAddTransient<IJsViewEngine, JsViewEngine>();
+            services.AddJsEngine(builder =>
+            {
+                // We are using polled here, simply because that is what this method did previously.
+                builder.UsePooledEngineFactory();
+                // Same thing as ^
+                builder.UseNodeRenderEngine();
+            });
         }
 
+        /// <summary>
+        /// Add the services required to use a render engine, a pool, etc.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="action"></param>
+        public static void AddJsEngine(this IServiceCollection services, Action<JsEngineServiceBuilder> action)
+        {
+            var builder = new JsEngineServiceBuilder();
+            action(builder);
+            builder.Register(services);
+        }
+        
         /// <summary>
         /// This will add <see cref="JsViewEngine"/> to a collection used by mvc to render
         /// </summary>
@@ -85,6 +101,97 @@ namespace JavaScriptViewEngine
 
                 var jsViewEngine = serviceProvider.GetRequiredService<IJsViewEngine>();
                 options.ViewEngines.Add(jsViewEngine);
+            }
+        }
+
+        public class JsEngineServiceBuilder
+        {
+            private Action<IServiceCollection> _engineFactoryAction;
+            private Action<IServiceCollection> _renderEngineBuilderAction;
+
+            internal JsEngineServiceBuilder() {  }
+
+            public JsEngineServiceBuilder UsePooledEngineFactory(Action<RenderPoolOptions> renderPoolOptionsSetupAction = null)
+            {
+                if(_engineFactoryAction != null)
+                    throw new Exception("You have already registered an engine factory.");
+
+                _engineFactoryAction = services =>
+                {
+                    services.TryAddTransient<IRenderEnginePool, RenderEnginePool>();
+                    services.TryAddSingleton<IRenderEngineFactory, PooledRenderEngineFactory>();
+                    services.Configure(renderPoolOptionsSetupAction);
+                };
+
+                return this;
+            }
+
+            public JsEngineServiceBuilder UseSingletonEngineFactory()
+            {
+                if (_engineFactoryAction != null)
+                    throw new Exception("You have already registered an engine factory.");
+
+                _engineFactoryAction = services =>
+                {
+                    services.TryAddSingleton<IRenderEngineFactory, SingletonRenderEngineFactory>();
+                };
+
+                return this;
+            }
+
+            public JsEngineServiceBuilder UseCustomEngineFactory<T>() where T: class, IRenderEngineFactory
+            {
+                if (_engineFactoryAction != null)
+                    throw new Exception("You have already registered an engine factory.");
+
+                _engineFactoryAction = services =>
+                {
+                    services.TryAddSingleton<IRenderEngineFactory, T>();
+                };
+
+                return this;
+            }
+
+            public JsEngineServiceBuilder UseNodeRenderEngine(Action<NodeRenderEngineOptions> nodeRenderOptionsAction = null)
+            {
+                if (_renderEngineBuilderAction != null)
+                    throw new Exception("You have already registered an engine builder.");
+
+                _renderEngineBuilderAction = services =>
+                {
+                    services.TryAddTransient<IRenderEngineBuilder, NodeRenderEngineBuilder>();
+                };
+
+                return this;
+            }
+
+            public JsEngineServiceBuilder UseRenderEngine<T>() where T: class, IRenderEngineBuilder
+            {
+                if (_renderEngineBuilderAction != null)
+                    throw new Exception("You have already registered an engine builder.");
+
+                _renderEngineBuilderAction = services =>
+                {
+                    services.TryAddTransient<IRenderEngineBuilder, T>();
+                };
+
+                return this;
+            }
+
+            internal void Register(IServiceCollection services)
+            {
+                if(_engineFactoryAction == null)
+                    throw new Exception("You must specific an engine factory using 'Use(.*)EngineFactory()' methods.");
+
+                if (_renderEngineBuilderAction == null)
+                    throw new Exception("You must specific an engine builder using 'Use(.*)RenderEngine()' methods.");
+
+                _engineFactoryAction(services);
+                _renderEngineBuilderAction(services);
+                
+                services.TryAddEnumerable(ServiceDescriptor.Transient<IConfigureOptions<MvcViewOptions>, JavaScriptViewEngineMvcViewOptionsSetup>());
+                services.TryAddTransient<IFileWatcher, FileWatcher>();
+                services.TryAddTransient<IJsViewEngine, JsViewEngine>();
             }
         }
 
