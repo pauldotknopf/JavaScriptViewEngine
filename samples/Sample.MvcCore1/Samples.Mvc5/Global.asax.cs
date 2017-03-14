0999cf74b69cfbeb6d104bcd6fa2b011e76bc392
Paul Knopf <pauldotknopf@gmail.com>
2017-03-14 05:44:52 -0400

    Added an empty Mvc5 project.
    Added a new csproject that will soon contain the DEFINES for Mvc5.

---
+++ samples/Sample.MvcCore1/Samples.Mvc5/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Samples.Mvc5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
