eaef4854f4d64a4406435afe6910fe1bc503c615
Paul Knopf <pauldotknopf@gmail.com>
2017-12-03 18:41:01 -0500

    Updating all packages in the MVC Core version.

---
+++ src/JavaScriptViewEngine.Mvc5/Options.cs
@@ -0,0 +1,17 @@
+﻿namespace JavaScriptViewEngine
+{
+    public interface IOptions<T>
+    {
+        T Value { get; }
+    }
+
+    public class Options<T> : IOptions<T>
+    {
+        public Options(T options)
+        {
+            Value = options;
+        }
+         
+        public T Value { get; private set; }
+    }
+}
